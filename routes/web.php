ed9845cf2758ca9ad68b72ee4fd4865e82ab1343
Miguel Vázquez <miguelvz26.mv@gmail.com>
2023-12-26 15:46:42 -0600

    splited contacts from personal

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,10 @@ use App\Http\Controllers\PaymentTicketController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\SupportController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -150,3 +140,8 @@ Route::resource('supports', SupportController::class)->middleware('auth');
 //comments routes-------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------
 Route::resource('comments', CommentController::class)->middleware('auth');
+
+
+//profile routes-------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+Route::post('users/{user}/update-personal', [UserController::class, 'updatePersonal'])->middleware('auth')->name('users.profile.update-personal');
