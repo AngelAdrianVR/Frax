78f7cf3cf7d57ae06aeac9c2c2aa635604394ac6
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-01-27 20:47:42 -0600

    mobile view common areas user (reservations)

--- resources/js/Pages/CommonAreaUser/Index.vue
+++ resources/js/Pages/CommonAreaUser/Index.vue
@@ -1,11 +1,11 @@
 <template>
     <AppLayout title="Áreas comunes">
-        <h1 class="font-bold mt-12 mx-20">Reservación de áreas</h1>
-        <p class="mt-6 mx-20 text-sm">En este apartado se puede visualizar las áreas comunes y se pueden reservar, es
+        <h1 class="font-bold mt-12 mx-5 lg:mx-20">Reservación de áreas</h1>
+        <p class="mt-6 mx-5 lg:mx-20 text-sm">En este apartado se puede visualizar las áreas comunes y se pueden reservar, es
             necesario leer
             el reglamento y
             las polícas establecidas para cada área. </p>
-        <div class="mt-4 mx-20 lg:grid grid-cols-3 gap-6">
+        <div class="mt-4 mx-5 lg:mx-20 lg:grid grid-cols-3 gap-6">
             <CommonAreaCard v-for="item in common_areas" :key="item.id" :commonArea="item" />
         </div>
     </AppLayout>
